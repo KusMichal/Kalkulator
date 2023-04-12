da6ef62eb0333c1dce976b0e4d5eee2bf567b665
Michal <kusmichal.ku@gmail.com>
2023-04-12 19:08:36 +0200

    funkcje_dodane

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,43 +1,53 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
-działanie = input("Padaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-try:
+def menu():
+    choice = input("Padaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    if choice > '4':
+        print("Wybierz poprawnie działanie")
+        logging.info(f'Niema takiego działania')
     a = eval(input("Podaj składnik 1: "))
     b = eval(input("Podaj składnik 2: "))
-except NameError:
-    print("Podaj poprawną liczbę")
-    logging.info(f'Błędna liczba')
-if działanie > '4':
-    print("Wybierz poprawnie działanie")
-    logging.info(f'Niema takiego działania')
+    result = calc(choice,a,b)
+    print(result)
 
 
-def wynik(a,b):
+def add(a,b):
+    logging.info(f'Dodaje {a} i {b}')
+    logging.info(f'Wynik to {a+b}')
+    return a+b
 
-    if działanie == '1':
-        logging.info(f'Dodaje {a} i {b}')
-        logging.info(f'Wynik to {a+b}')
-        return a+b
-    elif działanie == '2':
-        logging.info(f'Odejmuje {a} i {b}')     
-        logging.info(f'Wynik to {a-b}')
-        return a-b
-    elif działanie == '3':
-        logging.info(f'Mnoże {a} i {b}')
-        logging.info(f'Wynik to {a*b}')
-        return a*b
-    elif działanie == '4':
-        logging.info(f'Dziele {a} przez {b}')
-        if b == 0:
-            logging.error("nie dzielimy przez zero")
-        logging.info(f'Dziele {a} przez {b}')
-        logging.info(f'Wynik to {a/b}')
-        if wynik == 'Nie ma takiego działania':
-            logging.error('Nie ma takiego działania')
-        try: return a/b
-        except ZeroDivisionError:
-           return"Nie można dzielić przez 0"
-    else:       
-        return "Nie ma takiego działania"
+def subtract(a,b):
+    logging.info(f'Odejmuje {a} i {b}')     
+    logging.info(f'Wynik to {a-b}')
+    return a-b
+
+def multiply(a,b):
+    logging.info(f'Mnoże {a} i {b}')
+    logging.info(f'Wynik to {a*b}')
+    return a*b
+
+def divide(a,b):
+    logging.info(f'Dziele {a} przez {b}')
+    if b == 0:
+        logging.error("nie dzielimy przez zero")
+    logging.info(f'Dziele {a} przez {b}')
+    logging.info(f'Wynik to {a/b}')
+    try: return a/b
+    except ZeroDivisionError:
+        return"Nie można dzielić przez 0"
+def calc(choice,a,b):
+    choices = {'1':add, '2':subtract, '3':multiply, '4':divide}
+    method = choices.get(choice)
+    if method:
+        return method(a,b)
     
-print(wynik(a,b))
+    
+print(menu())
+
+    
+    
+  
+
+    
+ 
+
